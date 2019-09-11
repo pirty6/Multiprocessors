28eb0d6b2c09c77f20bb8dd8d77861f8ad650ec8
icecreamsoldier <mariana.9000@hotmail.com>
2019-09-10 22:09:31 -0500

    maybe?

--- MaxConsecutiveSum/MaxSum.java
+++ MaxConsecutiveSum/MaxSum.java
@@ -17,7 +17,7 @@ import java.util.Arrays;
 public class MaxSum extends Thread {
   private int [] array;
   private int start, end;
-  private long max;
+  private int max;
 
   public MaxSum(int start, int end, int[] array) {
     this.start = start;
@@ -26,43 +26,73 @@ public class MaxSum extends Thread {
     this.max = Integer.MIN_VALUE;
   }
 
-  public long getResult() {
+  public int getResult() {
     return max;
   }
 
-  public void run() {
-    for(int i = start; i <= end; i++) {
-      long curr_max = 0;
-      for(int j = i; j <= end; j++) {
-        curr_max += array[j];
-        max = Math.max(max, curr_max);
+  public int maxSubArray(int s, int e) {
+    if(s == e) {
+      return array[s];
+    }
+    int mid = s + (e - s) / 2;
+    int leftMaxSum = maxSubArray(s, mid);
+    int rightMaxSum = maxSubArray(mid + 1, e);
+
+    int sum = 0;
+    int leftMidMax = 0;
+    for(int i = mid; i >= s; i--) {
+      sum += array[i];
+      if(sum > leftMidMax) {
+        leftMidMax = sum;
       }
     }
+    sum = 0;
+    int rightMidMax = 0;
+    for(int i = mid + 1; i <= e; i++) {
+      sum += array[i];
+      if(sum > rightMidMax) {
+        rightMidMax = sum;
+      }
+    }
+    int centerSum = leftMidMax + rightMidMax;
+    return max = Math.max(centerSum, Math.max(leftMaxSum, rightMaxSum));
+  }
+
+  public void run() {
+    //System.out.println("Thread " + Thread.currentThread().getId() + " is running");
+    maxSubArray(start, end);
   }
 
   public static void main(String args[]) {
     final int SIZE = 1_000_000;
-    int[] a = new int[SIZE];
-    Utils.randomArray(a);
-    Utils.displayArray("Array", a);
+    //int[] a = new int[SIZE];
+    //Utils.randomArray(a);
+    int[] a =  {2, 3, 4, 5, 7};
+    //Utils.displayArray("Array", a);
+    //int aux = 0;
+    /*for(int i = 0; i < a.length; i++) {
+      aux += a[i];
+    }
+    System.out.println("Posible value: " + aux);*/
 
 
     MaxSum threads[];
     int block;
     long startTime, stopTime;
     double acum;
-    long result = Integer.MIN_VALUE;
+    int result = Integer.MIN_VALUE;
 
     threads = new MaxSum[Runtime.getRuntime().availableProcessors()];
     block = a.length / Runtime.getRuntime().availableProcessors();
 
     acum = 0;
+    int mid = (0 - a.length - 1) / 2;
     for(int i = 0; i < 10; i++) {
       for(int j = 0; j < threads.length; j++) {
         if(j != threads.length - 1) {
           threads[j] = new MaxSum((j + 1) * block, (j + 2) * block, a);
         } else {
-          threads[j] = new MaxSum((j + 1) * block, a.length - 1, a);
+          threads[j] = new MaxSum((j + 1) * block, a.length-1, a);
         }
       }
 
@@ -82,6 +112,7 @@ public class MaxSum extends Thread {
 
       for(int j = 0; j < threads.length; j++) {
         result = Math.max(result, threads[j].getResult());
+        //System.out.println(result);
       }
 
       stopTime = System.currentTimeMillis();
--- MaxConsecutiveSum/MaxSumSeq.java
+++ MaxConsecutiveSum/MaxSumSeq.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class MaxSumSeq {
   private int[] arr;
-  private long result;
+  private int result;
 
   public MaxSumSeq(int[] arr) {
     this.arr = arr;
@@ -13,34 +13,50 @@ public class MaxSumSeq {
     return result;
   }
 
-  public void maxSum() {
-    for(int i = 0; i < arr.length; i++) {
-      long curr_max = 0;
-      for(int j = i; j < arr.length; j++) {
-        curr_max += arr[j];
-        result = Math.max(result, curr_max);
-      }
+  public int maxSubArray(int s, int e) {
+    if(s == e){
+      return arr[s];
     }
+    int mid = s + (e - s)/2;
+    int leftMaxSum = maxSubArray(s, mid);
+    int rightMaxSum = maxSubArray(mid+1, e);
+
+    int sum = 0;
+    int leftMidMax =0;
+    for (int i = mid; i >=s ; i--) {
+        sum += arr[i];
+        if(sum>leftMidMax)
+            leftMidMax = sum;
+    }
+    sum = 0;
+    int rightMidMax =0;
+    for (int i = mid+1; i <= e ; i++) {
+        sum += arr [i];
+        if(sum>rightMidMax)
+            rightMidMax = sum;
+    }
+    int centerSum = leftMidMax + rightMidMax;
+    return result = Math.max(centerSum, Math.max(leftMaxSum, rightMaxSum));
   }
 
-    public static void main(String args[]) {
-      final int SIZE = 1_000_000;
-      int[] a = new int[SIZE];
-      //int [] a = {-1, 6, -2, 5, -1, 4, 3, -4, -3, 1};
-      Utils.randomArray(a);
-      Utils.displayArray("Array", a);
+  public static void main(String args[]) {
+    final int SIZE = 1_000_000;
+    int[] a = new int[SIZE];
+    //int [] a = {-1, 6, -2, 5, -1, 4, 3, -4, -3, 1};
+    Utils.randomArray(a);
+    Utils.displayArray("Array", a);
 
-      long startTime, stopTime;
-      double acum = 0;
-      MaxSumSeq obj = new MaxSumSeq(a);
-      for(int i = 0; i < 10; i++) {
-        startTime = System.currentTimeMillis();
-        obj.maxSum();
-        stopTime = System.currentTimeMillis();
-        acum += (stopTime - startTime);
-      }
-
-      System.out.println("result = " + obj.getResult());
-      System.out.println("avg time = " + (acum / 10));
+    long startTime, stopTime;
+    double acum = 0;
+    int result = 0;
+    MaxSumSeq obj = new MaxSumSeq(a);
+    for(int i = 0; i < 10; i++) {
+      startTime = System.currentTimeMillis();
+      obj.maxSubArray(0, a.length - 1);
+      stopTime = System.currentTimeMillis();
+      acum += (stopTime - startTime);
     }
+    System.out.println("result = " + obj.getResult());
+    System.out.println("avg time = " + (acum / 10));
+  }
 }
